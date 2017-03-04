9595d9f8983ce984af396743502176e8476af021
Menghan <somuchsentimentality112@gmail.com>
2017-03-03 21:18:41 -0500

    Update DiscreteEnvironment.py

--- code/DiscreteEnvironment.py
+++ code/DiscreteEnvironment.py
@@ -27,6 +27,8 @@ class DiscreteEnvironment(object):
         # space to a node in discrete space
         #
         node_id = 0
+        coord = self.ConfigurationToGridCoord(config)
+        node_id = self.GridCoordToNodeId(coord)
         return node_id
 
     def NodeIdToConfiguration(self, nid):
@@ -36,6 +38,8 @@ class DiscreteEnvironment(object):
         # in the full configuration space
         #
         config = [0] * self.dimension
+        coord = self.NodeIdToGridCoord(nid)
+        config = self.GridCoordToConfiguration(coord)
         return config
         
     def ConfigurationToGridCoord(self, config):
@@ -45,6 +49,9 @@ class DiscreteEnvironment(object):
         # to a grid coordinate in discrete space
         #
         coord = [0] * self.dimension
+        # for i  in range (self.dimension):
+        coord = numpy.floor((config-self.lower_limits)/self.resolution)
+
         return coord
 
     def GridCoordToConfiguration(self, coord):
@@ -54,6 +61,7 @@ class DiscreteEnvironment(object):
         # to a configuration in the full configuration space
         #
         config = [0] * self.dimension
+        config = (coord+0.5)*self.resolution + self.lower_limits
         return config
 
     def GridCoordToNodeId(self,coord):
@@ -62,7 +70,12 @@ class DiscreteEnvironment(object):
         # This function maps a grid coordinate to the associated
         # node id 
         node_id = 0
+
+        for i in range(len(coord)):
+            node_id = node_id + coord[i]*numpy.power(self.num_cells[i],i)
+
         return node_id
+
 
     def NodeIdToGridCoord(self, node_id):
         
@@ -70,6 +83,10 @@ class DiscreteEnvironment(object):
         # This function maps a node id to the associated
         # grid coordinate
         coord = [0] * self.dimension
+        # n = numpy.floor((self.upper_limits - self.lower_limits) / self.resolution)
+        for i in range(self.dimension-1,-1,-1):
+            coord[i] = numpy.floor(node_id/numpy.power(self.num_cells[i],i))
+            node_id =  numpy.mod(node_id,numpy.power(self.num_cells[i],i))
         return coord
         
         
