26acd2f28ece16b77a1d5dd0e8883d6c0875841a
likai28 <likai28@126.com>
2017-03-05 12:08:08 -0500

    bfs and dfs done

--- code/BreadthFirstPlanner.py
+++ code/BreadthFirstPlanner.py
@@ -1,4 +1,4 @@
-
+from collections import deque
 class BreadthFirstPlanner(object):
     
     def __init__(self, planning_env, visualize):
@@ -13,8 +13,50 @@ class BreadthFirstPlanner(object):
         #  The return path should be a numpy array
         #  of dimension k x n where k is the number of waypoints
         #  and n is the dimension of the robots configuration space
-        
-        plan.append(start_config)
-        plan.append(goal_config)
-   
+
+        #Build a dict to record vaild path
+        path = dict()
+        #Get the start and end node
+        start_node = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
+        goal_node = self.planning_env.discrete_env.ConfigurationToNodeId(goal_config)
+        #Build the queue to store nodes
+        queue = deque([])
+        #Add the start node into queue
+        queue.append(start_node)
+        #Build a dict to store node and visited state
+        is_visited = dict()
+        is_visited[start_node]=True
+        while len(queue)!=0:
+            x = queue.popleft()
+            if x == goal_node:
+            # This if only for start_node is goal_node initially
+                plan = self.find_path(path,start_node,goal_node)
+                return plan
+            else:
+                nodes_nearby = self.planning_env.GetSuccessors(x)
+                for node in nodes_nearby:
+                    if node in is_visited and is_visited[node]==True:
+                        continue
+                    else:
+                        is_visited[node]=True
+                        queue.append(node)
+                        #Add the valid edge into path
+                        path[node]= x
+                        #If this node reach goal_node, get the whole plan
+                        if node == goal_node:
+                            plan = self.find_path(path,start_node,goal_node)
+                            break
+
         return plan
+    #This help function is used to get the path in bfs
+    def find_path(self, path, start_id, end_id): #[start_id, end_id)
+        id_next_v = end_id
+        path2 = []
+        node_config = self.planning_env.discrete_env.NodeIdToConfiguration(id_next_v)
+        path2.append(node_config)
+        while(id_next_v != start_id):
+            id_next_v = path[id_next_v]
+            node_config = self.planning_env.discrete_env.NodeIdToConfiguration(id_next_v)
+            path2.append(node_config)
+        path2.reverse()
+        return path2
--- code/DepthFirstPlanner.py
+++ code/DepthFirstPlanner.py
@@ -8,13 +8,55 @@ class DepthFirstPlanner(object):
     def Plan(self, start_config, goal_config):
         
         plan = []
-        
-        # TODO: Here you will implement the depth first planner
+
+        # TODO: Here you will implement the breadth first planner
         #  The return path should be a numpy array
         #  of dimension k x n where k is the number of waypoints
         #  and n is the dimension of the robots configuration space
 
-        plan.append(start_config)
-        plan.append(goal_config)
+        #Build a dict to record vaild path
+        path = dict()
+        #Get the start and end node
+        start_node = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
+        goal_node = self.planning_env.discrete_env.ConfigurationToNodeId(goal_config)
+        #Build the queue to store nodes
+        stack = []
+        #Add the start node into queue
+        stack.append(start_node)
+        #Use the nodes dict to store node and visited state
+        self.nodes[start_node]=True
+        while len(stack)!=0:
+            x = stack.pop()
+            if x == goal_node:
+            # This if only for start_node is goal_node initially
+                plan = self.find_path(path,start_node,goal_node)
+                return plan
+            else:
+                nodes_nearby = self.planning_env.GetSuccessors(x)
+                for node in nodes_nearby:
+                    if node in self.nodes and self.nodes[node]==True:
+                        continue
+                    else:
+                        self.nodes[node]=True
+                        stack.append(node)
+                        #Add the valid edge into path
+                        path[node]= x
+                        #If this node reach goal_node, get the whole plan
+                        if node == goal_node:
+                            plan = self.find_path(path,start_node,goal_node)
+                            break
 
         return plan
+
+
+    def find_path(self, path, start_id, end_id): #[start_id, end_id)
+        id_next_v = end_id
+        path2 = []
+        node_config = self.planning_env.discrete_env.NodeIdToConfiguration(id_next_v)
+        path2.append(node_config)
+        while(id_next_v != start_id):
+            id_next_v = path[id_next_v]
+            node_config = self.planning_env.discrete_env.NodeIdToConfiguration(id_next_v)
+            path2.append(node_config)
+        path2.reverse()
+        return path2
